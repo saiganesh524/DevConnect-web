1ef2e53a7546bbe119e7978c7979edecc33c8830
saiganesh524 <saiganeshkandula93@gmail.com>
2024-09-28 12:30:34 +0530

    setup redux store

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
+import Feed from "./Feed";
+// import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+}
+
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("saiganesh@gmail.com");
   const [password, setPassword] = useState("Ganesh@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
-      await axios.post(
-        "http://localhost:3000/login",
+      const res = await axios.post(
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
+      // console.log(res.data);
+      dispatch(addUser(res.data));
+      navigate("/");
     } catch (e) {
       console.error(e);
     }
