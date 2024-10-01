a50bc0d927ff6994fbae84721b16b15a90e4b8e0
saiganesh524 <saiganeshkandula93@gmail.com>
2024-10-01 16:01:41 +0530

    Connections Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Body from "./Body";
-import Login from "./Login";
-import Profile from "./Profile";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 import { Provider } from "react-redux";
-import appStore from "../utils/appStore";
-import Feed from "./Feed";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
               <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Profile />} />
             </Route>
           </Routes>
         </BrowserRouter>
